c22e375d25d9b5323a4f3980d66751ddba176bb0
Kalid Nesredin <98024840+kalidnesredin@users.noreply.github.com>
2023-09-02 05:52:51 +0300

    Add files via upload

---
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -0,0 +1,36 @@
+function valideForm() {
+    let estValide = true;
+    const emailInput = document.getElementById("email");
+    if (emailInput.value === "") {
+      document.getElementById("error_message").innerHTML = "Valid Email Required";
+      innerHTML.style.color = "red"
+      emailInput.nextElementSibling.style.display = "block";
+      emailInput.style.border = "1px solid red";
+      estValide = false;
+    } else {
+      emailInput.nextElementSibling.style.display = "none";
+      emailInput.style.border = "3px solid black";
+    }
+  
+    if (estValide) {
+      const emailPlaceholder = document.getElementById("email_placeholder");
+      emailPlaceholder.textContent = emailInput.value;
+      document.getElementById("success_message").style.display = "block";
+    }
+    function valideForm()
+{
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(emailInput.value.match(mailformat))
+    {
+    alert("Valid email address!");
+    document.getElementById("error_message").focus();
+        return true;
+    }
+    else
+    {
+    alert("You have entered an invalid email address!");
+    document.form1.text1.focus();
+    return false;
+    }
+    }
+    }
